8556f3a857e8be3d2fb30e92bcbd46853d7dc2b1
Nicolas Garcia Sastre <n.garcia.sastre@gmail.com>
2024-03-24 01:11:58 +0100

    fixed url

--- content_script.js
+++ content_script.js
@@ -1,27 +1,51 @@
-let isProductPage = document.getElementsByClassName('main no-outline').length > 0;
+let isProductPage =
+  document.getElementsByClassName("main no-outline").length > 0;
 
-let downloadModels = (models) => {
-    return () => {
-        for (let model of models) {
-            window.open(model.url);
-        }
+let downloadModels = (URLs) => {
+  return () => {
+    for (let url of URLs) {
+      window.open(url);
     }
-}
+  };
+};
 
 if (isProductPage) {
-    let productId = document.querySelector("#content > div > div > div > div.pip-product__subgrid.product-pip.js-product-pip").getAttribute('data-product-no');
-    let buttonContainer = document.getElementById("pip-buy-module-content");
-
-    fetch(`https://www.ikea.com/global/assets/rotera/resources/${productId}.json`)
-        .then((data) => data.json())
-        .then((data) => {
-            let button = document.createElement('a');
-            button.innerHTML = 'Download 3D model';
-            button.style.cursor = 'pointer';
-            button.style.textDecoration = 'underline';
-            if (data.models.length > 0) {
-                button.onclick = downloadModels(data.models);
-                buttonContainer.insertBefore(button, buttonContainer.childNodes[1]);
-            }
-        });
+  let productId = document
+    .querySelector(
+      "#content > div > div > div > div.pip-product__subgrid.product-pip.js-product-pip"
+    )
+    .getAttribute("data-product-no");
+  let buttonContainer = document.getElementById("pip-buy-module-content");
+  fetch(
+    `https://web-api.ikea.com/dimma/statics/${productId}?vars=show&market=es&loc=es`,
+    {
+      credentials: "omit",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
+        Accept: "application/json;version=2",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/json",
+        "x-consumer-id": "1",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+      },
+      referrer: "https://www.ikea.com/",
+      method: "GET",
+      mode: "cors",
+    }
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      if (!data.modelUrl) {
+        return;
+      }
+      let button = document.createElement("a");
+      button.innerHTML = "Download 3D model";
+      button.style.cursor = "pointer";
+      button.style.textDecoration = "underline";
+      button.onclick = downloadModels([data.modelUrl]);
+      buttonContainer.insertBefore(button, buttonContainer.childNodes[1]);
+    });
 }
